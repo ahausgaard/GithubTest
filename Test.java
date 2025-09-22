08cb232dd3fbbd0e7d2d6e05d16f7c2f457a5d14
Andreas Hausgaard Jakobsen <ahausgaardjakobsen@gmail.com>
2025-09-22 10:12:12 +0200

    Løst merge-konflikt inkl. kommentar

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test{
     public static void main(String[] args){
+    //Nu er begge ændringer med.
     System.out.println("Ændring fra VSCode, som skaber konflikt!");
     System.out.println("Hello from IntelliJ!");
     }
