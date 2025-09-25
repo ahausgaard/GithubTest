442f259f83c35a9f760a74fc7c5b0a86e617c8b2
Andreas Hausgaard Jakobsen <ahausgaardjakobsen@gmail.com>
2025-09-25 11:14:17 +0200

    Comitt fra VSCode

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test{
     public static void main(String[] args){
     //Nu er begge ændringer med.
     System.out.println("Ændring fra VSCode, som skaber konflikt!");
-    System.out.println("Hello from IntelliJ!");
+    System.out.println("Hello from - nej fra VSCode!");
     }
 }
